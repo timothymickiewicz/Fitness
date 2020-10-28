d49ed8730ae3abe5635a23fc261976d64aee9198
Timothy Mickiewicz <timothy.mickiewicz@gmail.com>
2020-10-28 00:10:15 -0500

    create exercise tab created and styled for all screen sizes

--- src/components/CreateExercise/CreateExercise.js
+++ src/components/CreateExercise/CreateExercise.js
@@ -1,11 +1,34 @@
 import React from 'react';
 import './CreateExercise.css';
 
+import Chest from '../../assets/icons/chest.png'
+import Back from '../../assets/icons/back.png'
+import Arms from '../../assets/icons/arms.png'
+import Legs from '../../assets/icons/legs.png'
+import Abs from '../../assets/icons/abs.png'
+import Cardio from '../../assets/icons/cardio.png'
+
+const icons = [
+    Chest,
+    Back,
+    Arms,
+    Legs,
+    Abs,
+    Cardio
+]
+
 function CreateExercise(props) {
     
     return (
-        <div>
-            createExercise
+        <div className="exerciseContainer row">
+            {icons.map((key, index) => {
+                console.log(key)
+                return (
+                    <button className="iconWrap" type="submit">
+                        <img className="iconItem" src={key} alt={"type of workout is" + key}></img>
+                    </button>
+                )
+            })}
         </div>
     )
 }
