178ee1a28cf98e7ddc6ebd1dfe6cd050e472e0bc
Timothy Mickiewicz <timothy.mickiewicz@gmail.com>
2020-11-07 01:11:26 -0600

    reduced workout renderings with new local storage for entry fields

--- client/src/components/AddWorkout/Workout.js
+++ client/src/components/AddWorkout/Workout.js
@@ -6,20 +6,65 @@ import $ from 'jquery';
 import API from '../../utils/API';
 
 function Workout(props) {
-  //   Pulled from the workout table
+  //   Allows rendering to next page after clicking continue or new workout
   const [workoutUUID, setWorkoutUUID] = React.useState('');
-  //   Posts to the workoutExercises table, relates to workout entry
+  //   Used to generate sets fields dynamically
   const [sets, setSets] = React.useState(0);
-  const [duration, setDuration] = React.useState('');
-  const [exerciseName, setExerciseName] = React.useState('');
-  const [exerciseType, setExerciseType] = React.useState('');
-  //   Posts to the sets table, relates to workoutExercises entry
+  //   Template literals the sets and their associated weights
   let [setWeights, setSetWeights] = React.useState('');
 
-  React.useEffect(() => {}, [props.listOfExercises]);
+  React.useEffect(() => {
+    if (localStorage.getItem('workoutUUID')) {
+      setWorkoutUUID(localStorage.getItem('workoutUUID'));
+    }
+    // Gets localstorage data after returning to tab, sets field values accordingly
+    $('#exercises').val(localStorage.getItem('exercise'));
+    $('#setSets').val(JSON.parse(localStorage.getItem('sets')));
+    setSets(JSON.parse(localStorage.getItem('sets')));
+    $('#breakDuration').val(JSON.parse(localStorage.getItem('duration')));
+    for (let i = 1; i <= JSON.parse(localStorage.getItem('sets')); i++) {
+      $('#setsFieldReps' + i).val(
+        JSON.parse(localStorage.getItem(`setsFieldReps${i}`))
+      );
+      $('#setsFieldWeight' + i).val(
+        JSON.parse(localStorage.getItem(`setsFieldWeight${i}`))
+      );
+    }
 
-  //   Sets the form data to state
-  const handleSetWeights = () => {
+    // Sets localstorage data after leaving tab
+    return function setStorage() {
+      for (let i = 1; i <= $('#setSets').val(); i++) {
+        localStorage.setItem(
+          `setsFieldReps${i}`,
+          JSON.stringify($('#setsFieldReps' + i).val())
+        );
+        localStorage.setItem(
+          `setsFieldWeight${i}`,
+          JSON.stringify($('#setsFieldWeight' + i).val())
+        );
+      }
+    };
+  }, [props.listOfExercises, workoutUUID]);
+
+  const handleGetLastWorkout = () => {
+    API.getLastWorkout()
+      .then(res => {
+        setWorkoutUUID(res.data.workout_id);
+        localStorage.setItem('workoutUUID', res.data.workout_id);
+      })
+      // replace this with alert
+      .catch(err => alert('no previous workout'));
+  };
+
+  const handleStartNewWorkout = () => {
+    API.startWorkout().then(res => {
+      setWorkoutUUID(res.data.workout_id);
+      localStorage.setItem('workoutUUID', res.data.workout_id);
+    });
+  };
+
+  //   Formats set data, creates workoutExercise, clears fields and localstorage
+  const handleSubmitWorkout = () => {
     for (let i = 1; i <= sets; i++) {
       setSetWeights(
         (setWeights += `{"reps": ${$(
@@ -27,29 +72,20 @@ function Workout(props) {
         ).val()}, "weight": ${$('#setsFieldWeight' + i).val()}},`)
       );
     }
-  };
-
-  const handleClearFields = () => {
-    $('.inputBoxSetsField').val('');
-    $('#exercises').val('Select an Option');
-    $('#setSets').val('');
-    setSets(0);
-    $('#breakDuration').val('');
-    $('.openingContainer').css('display', '');
-    $('.content').css('display', 'none');
-  };
-
-  //   Submits the workout, returns UUID of workoutExercise to post the set data
-  const handleSubmitWorkout = () => {
     API.createWorkoutExercise({
-      exerciseName: exerciseName,
-      exerciseType: exerciseType,
-      numOfSets: parseInt(sets),
-      breakDuration: parseInt(duration),
+      exerciseName: localStorage.getItem('exercise'),
+      exerciseType: localStorage.getItem('type'),
+      numOfSets: JSON.parse(localStorage.getItem('sets')),
+      breakDuration: JSON.parse(localStorage.getItem('duration')),
       setWeights: setWeights,
-      WorkoutWorkoutId: workoutUUID,
+      WorkoutWorkoutId: localStorage.getItem('workoutUUID'),
     }).then(res => {
-      console.log(res);
+      localStorage.clear();
+      $('.inputBoxSetsField').val('');
+      $('#exercises').val('Select an Option');
+      $('#setSets').val('');
+      setSets(0);
+      $('#breakDuration').val('');
     });
   };
 
@@ -81,100 +117,116 @@ function Workout(props) {
   return (
     <div className='row workoutContainer'>
       <div className='workoutHeader'>Workout</div>
-      {/* Buttons to continue workout or start new */}
-      <div className='row openingContainer'>
-        <button
-          id='continue'
-          className='introBtn'
-          onClick={() => {
-            API.getLastWorkout()
-              .then(res => {
-                setWorkoutUUID(res.data.workout_id);
-                $('.openingContainer').css('display', 'none');
-                $('.content').css('display', 'flex');
-              })
-              // Will create an alert at top level that takes over the center of the viewport to say nothing exists
-              .catch(err => alert('no previous workout'));
-          }}>
-          Continue
-        </button>
-        <button
-          id='new'
-          className='introBtn'
-          onClick={() => {
-            API.startWorkout().then(res => {
-              setWorkoutUUID(res.data.workout_id);
-              $('.openingContainer').css('display', 'none');
-              $('.content').css('display', 'flex');
-            });
-          }}>
-          New
-        </button>
-      </div>
-      <div className='row content'>
-        <div className='row section'>
-          <div className='col-12 sectionHeader'>Exercise:</div>
-          <select
-            onChange={e => {
-              setExerciseName(e.target.value);
-              setExerciseType(
-                e.target.options[e.target.selectedIndex].dataset.type
-              );
-            }}
-            name='exercises'
-            id='exercises'>
-            <option key={0} data-type={'empty'} defaultValue='Select an Option'>
-              Select an Option
-            </option>
-            {props.listOfExercises.join() !== '' ? (
-              props.listOfExercises.map((key, index) => {
-                return (
-                  <option key={index} data-type={key.type} value={key.name}>
-                    {key.name}
-                  </option>
+      {workoutUUID === '' ? (
+        <div className='row openingContainer'>
+          <button
+            id='continue'
+            className='introBtn'
+            onClick={() => {
+              handleGetLastWorkout();
+            }}>
+            Continue
+          </button>
+          <button
+            id='new'
+            className='introBtn'
+            onClick={() => {
+              handleStartNewWorkout();
+            }}>
+            New
+          </button>
+        </div>
+      ) : (
+        <div className='row content'>
+          <div className='row section'>
+            <div className='col-12 sectionHeader'>Exercise:</div>
+            <select
+              onChange={e => {
+                if (localStorage.getItem('exercise')) {
+                  localStorage.removeItem('exercise');
+                }
+                localStorage.setItem('exercise', e.target.value);
+                if (localStorage.getItem('type')) {
+                  localStorage.removeItem('type');
+                }
+                localStorage.setItem(
+                  'type',
+                  e.target.options[e.target.selectedIndex].dataset.type
                 );
-              })
-            ) : (
+              }}
+              name='exercises'
+              id='exercises'>
               <option
-                id='exercises'
                 key={0}
-                disabled={true}
-                value='No Workouts Created'>
-                No Workouts Created
+                data-type={'empty'}
+                value=''
+                defaultValue=''
+                disabled
+                hidden>
+                Select an Option
               </option>
-            )}
-          </select>
-        </div>
-        <div className='row section'>
-          <div className='col-12 sectionHeader'>Sets:</div>
-          <input
-            id='setSets'
-            onChange={e => setSets(e.target.value)}
-            className='inputBox'></input>
+              {props.listOfExercises.join() !== '' ? (
+                props.listOfExercises.map((key, index) => {
+                  return (
+                    <option key={index} data-type={key.type} value={key.name}>
+                      {key.name}
+                    </option>
+                  );
+                })
+              ) : (
+                <option
+                  id='exercises'
+                  key={0}
+                  disabled={true}
+                  value='No Workouts Created'>
+                  No Workouts Created
+                </option>
+              )}
+            </select>
+          </div>
+          <div className='row section'>
+            <div className='col-12 sectionHeader'>Sets:</div>
+            <input
+              id='setSets'
+              onChange={e => {
+                if (localStorage.getItem('sets')) {
+                  localStorage.removeItem('sets');
+                }
+                localStorage.setItem('sets', JSON.stringify(e.target.value));
+                setSets(e.target.value);
+              }}
+              className='inputBox'></input>
 
-          {/* Sets field generates here */}
-          {sets !== 0 ? (
-            <div className='setsFieldContainer'>{calcSetFields()}</div>
-          ) : null}
+            {/* Sets field generates here */}
+            {sets !== 0 ? (
+              <div className='setsFieldContainer'>{calcSetFields()}</div>
+            ) : null}
+          </div>
+          <div className='row section'>
+            <div className='col-12 sectionHeader'>Break Duration:</div>
+            <input
+              id='breakDuration'
+              placeholder='seconds'
+              onChange={e => {
+                if (localStorage.getItem('duration')) {
+                  localStorage.removeItem('duration');
+                }
+                localStorage.setItem(
+                  'duration',
+                  JSON.stringify(e.target.value)
+                );
+              }}
+              className='inputBox'></input>
+          </div>
+          <button
+            className='submitWorkout'
+            onClick={() => {
+              handleSubmitWorkout();
+            }}>
+            Submit
+          </button>
         </div>
-        <div className='row section'>
-          <div className='col-12 sectionHeader'>Break Duration:</div>
-          <input
-            id='breakDuration'
-            placeholder='seconds'
-            onChange={e => setDuration(e.target.value)}
-            className='inputBox'></input>
-        </div>
-        <button
-          className='submitWorkout'
-          onClick={() => {
-            handleSetWeights();
-            handleSubmitWorkout();
-            handleClearFields();
-          }}>
-          Submit
-        </button>
-      </div>
+      )}
     </div>
   );
 }
